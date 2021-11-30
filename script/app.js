d5e4fcd047979d090ff1b0869f6b1628fd2284bd
AnastaSAV <anastasia.personalbox@gmail.com>
2021-11-30 12:30:04 +0200

    hw3 done

---
+++ script/app.js
@@ -0,0 +1,8 @@
+let firstNumber = prompt('Enter any number');
+let secondNumber = prompt('Enter any number again');
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+// alert(Number(firstNumber) + Number(secondNumber)); //Работает
+// alert(parseInt(firstNumber + secondNumber)); // И так работает
+let result = firstNumber + secondNumber;
+alert(`Result is ${result}`) // Этот вариант кажется более правильным
